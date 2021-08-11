a3cf346eadffb2196ddf77cbaaa5103e86963e76
Henry Addison <henryaddison@users.noreply.github.com>
2021-08-11 13:27:04 +0100

    add a job script for running regrid script on BP

---
+++ regrid-job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#
+#PBS -l select=1:ncpus=1:mem=500M
+#PBS -l walltime=0:0:10
+
+conda activate bpdownscaling
+
+set -euo pipefail
+
+cd $HOME/code/ml-downscaling-emulation
+
+python regrid-gcm.py --data /bp1store/geog-tropical/data/UKCP18/ --derived  /bp1store/geog-tropical/derived_data/UKCP18/
