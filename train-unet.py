e2c0307f2db0bef621b3134a776af0b4d0121acd
Henry Addison <henryaddison@users.noreply.github.com>
2021-10-27 10:52:57 +0100

    make sure the checkpoints directory exists
    to save models in

--- train-unet.py
+++ train-unet.py
@@ -55,6 +55,8 @@ if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO, format='%(levelname)s %(asctime)s: %(message)s')
 
+    os.makedirs(args.model_checkpoints_dir, exist_ok=True)
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     logging.info(f'Using device {device}')
 
