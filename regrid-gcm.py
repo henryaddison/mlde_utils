7d1df9380aa8d512e2cc3154ed5e617dbe328af2
Henry Addison <henryaddison@users.noreply.github.com>
2021-11-05 16:32:15 +0000

    update regrid script to use the new Regrid class

--- regrid-gcm.py
+++ regrid-gcm.py
@@ -6,6 +6,8 @@ import os
 import cftime
 import iris
 
+from preprocessing.regrid import Regrid
+
 variables = ["pr", "psl"]
 # years = itertools.chain(range(1980, 2000), range(2020, 2040), range(2060, 2080))
 years = range(1980, 1982)
@@ -23,10 +25,15 @@ def gcm_file(year, var):
 def get_args():
     parser = argparse.ArgumentParser(description='Regrid GCM data to match the CPM data',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--data', dest='data_dir', type=str, required=True,
+    parser.add_argument('--input', dest='input_dir', type=str, required=True,
+                        help='Path to directory holding raw files')
+    parser.add_argument('--target', dest='target_file', type=str, required=True,
                         help='Base path to raw data storage')
-    parser.add_argument('--derived', dest='derived_data', type=str, required=True,
-                        help='Base path to storage for derived data')
+    parser.add_argument('--output', dest='output_dir', type=str, required=True,
+                        help='Path to directory to store regridded files')
+    parser.add_argument('--scheme', dest='regrid_scheme', type=str, required=False,
+                        choices=Regrid.SCHEMES.keys(), default='linear',
+                        help='Regridding scheme')
 
     return parser.parse_args()
 
@@ -34,23 +41,8 @@ def get_args():
 if __name__ == '__main__':
     args = get_args()
 
-    cpm_files = glob.glob(f"{args.data_dir}/2.2km/rcp85/01/pr/day/*.nc")
+    os.makedirs(args.output_dir, exist_ok=True)
 
-    cpm_cube = iris.load_cube(cpm_files[0])
+    outputs = Regrid(args.input_dir, args.target_file, args.output_dir, args.regrid_scheme).run()
 
-    for var in variables:
-        gcm_files = f"{args.data_dir}/60km/rcp85/01/{var}/day/*19*.nc"
-
-        output_dir = f"{args.derived_data}/60km-2.2km-regrid-lin/rcp85/01/{var}/day"
-        os.makedirs(output_dir, exist_ok=True)
-
-        for year in years:
-            gcm_cube = iris.load_cube(gcm_file(year, var))
-
-            single_year_gcmcube = gcm_cube.extract(iris.Constraint(time=lambda cell: cftime.Datetime360Day(year, 12, 1, 12, 0, 0, 0) <= cell.point <= cftime.Datetime360Day(year+1, 11, 30, 12, 0, 0, 0)))
-
-            rp_gcm_cube = single_year_gcmcube.regrid(cpm_cube, iris.analysis.Linear())
-
-            iris.save(rp_gcm_cube, f"{output_dir}/{var}_rcp85_land-gcm_uk_60km_01_day_rp_regrid_{year}1201-{year+1}1130.nc")
-
-    print("All done")
+    print(outputs)
