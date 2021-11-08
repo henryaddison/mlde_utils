0e5e491157e0761d8ab57f007f4715fe35273dce
Henry Addison <henryaddison@users.noreply.github.com>
2021-11-08 12:17:52 +0000

    switch subregion selection to use actual values rather than index values
    it's a bit safer (or is it more dangerous, I guess the index version will make it clear the size of the image

--- preprocessing/select_subregion.py
+++ preprocessing/select_subregion.py
@@ -8,8 +8,11 @@ logger = logging.getLogger(__name__)
 
 class SelectSubregion:
 
-    LONDON_IN_CPM = {"grid_longitude": range(331, 391), "grid_latitude": range(139,199)}
-    LONDON_IN_GCM = {"projection_x_coordinate": [12, 13], "projection_y_coordinate": [4, 5]}
+    # expected index ranges for LONDON_IN_CPM {"grid_latitude": range(142, 196), "grid_longitude": range(337, 391)}
+    LONDON_IN_CPM = {"grid_latitude": slice(-1.51995003, -0.45995), "grid_longitude": slice(361.03076172, 362.09075928)}
+
+    # expected index ranges for LONDON_IN_GCM {"projection_x_coordinate": [12, 13], "projection_y_coordinate": [4, 5]}
+    LONDON_IN_GCM = {"projection_x_coordinate": slice(510000., 570000.), "projection_y_coordinate": slice(150000., 210000.)}
 
     def __init__(self, input_dir, output_dir, subregion_defn) -> None:
         self.input_dir = input_dir
@@ -26,7 +29,7 @@ class SelectSubregion:
 
             output_filepath = self.output_dir / f"{os.path.basename(full_file)}"
 
-            subregion_da = da.isel(self.subregion_defn)
+            subregion_da = da.sel(self.subregion_defn)
             subregion_da.to_netcdf(output_filepath)
             output_files.append(output_filepath)
 
