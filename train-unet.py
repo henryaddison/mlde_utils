fffe3f898c49d273147d70acdba7439de1a1d712
Henry Addison <henryaddison@users.noreply.github.com>
2021-11-09 15:36:37 +0000

    DRY train scripts
    and tidy up some of the tracking and training code into functions

--- train-unet.py
+++ train-unet.py
@@ -1,20 +1,23 @@
-import argparse, logging, os, sys
+import argparse
+import logging
+import os
 from pathlib import Path
+import sys
+
+import torch
 dir2 = os.path.abspath('unet/unet')
 dir1 = os.path.dirname(dir2)
 if not dir1 in sys.path: sys.path.append(dir1)
 import unet
-import torch
-from torch.utils.data import random_split, DataLoader, TensorDataset
 
-import numpy as np
+from training import train, load_data, log_epoch, track_run, checkpoint_model
 
-import wandb
-from mlflow import log_metric, log_param, log_artifacts, set_experiment, set_tags
-from torch.utils.tensorboard import SummaryWriter
+ARCHITECTURE="U-Net"
+EXPERIMENT_NAME="ml-downscaling-emulator"
+TAGS = ["baseline", ARCHITECTURE]
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Train U-Net to downscale',
+    parser = argparse.ArgumentParser(description=f'Train {ARCHITECTURE} to downscale',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--loss', '-l', dest='loss', type=str, default='l1', help='Loss function')
     parser.add_argument('--data', dest='data_dir', type=Path, required=True,
@@ -23,66 +26,9 @@ def get_args():
                         help='Base path to storage for models')
     parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
     parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=4, help='Batch size')
+    parser.add_argument('--lr', dest='learning_rate', type=float, default=2e-4, help='Learning rate')
 
     return parser.parse_args()
-
-def train_epoch(model, dataloader, device, epoch):
-    model.train()
-
-    epoch_loss = 0.0
-
-    for i, (batch_X, batch_y) in enumerate(dataloader):
-        loss = train_on_batch(batch_X.to(device), batch_y.to(device), model)
-        epoch_loss += loss.item()
-
-        # Log progress at least every 10th batch
-        if (len(dataloader) <= 10) or ((i+1) % max(len(dataloader)//10,1) == 0):
-            logging.info(f"Epoch {epoch}: Batch {i}: Batch Train Loss {loss.item()} Running Epoch Train Loss {epoch_loss}")
-
-    return epoch_loss
-
-def val_epoch(model, dataloader, device, epoch):
-    model.eval()
-
-    epoch_val_loss = 0
-    for batch_X, batch_y in dataloader:
-        val_loss = val_on_batch(batch_X.to(device), batch_y.to(device), model)
-
-        # Progress
-        epoch_val_loss += val_loss.item()
-
-    model.train()
-
-    return epoch_val_loss
-
-def train_on_batch(batch_X, batch_y, model):
-    # Compute prediction and loss
-    outputs_tensor = model(batch_X)
-    loss = criterion(outputs_tensor, batch_y)
-
-    # Backpropagation
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-    return loss
-
-def val_on_batch(batch_X, batch_y, model):
-    with torch.no_grad():
-        # Compute prediction and loss
-        outputs_tensor = model(batch_X)
-        loss = criterion(outputs_tensor, batch_y)
-
-    return loss
-
-def load_data(data_dirpath, batch_size):
-    train_set = TensorDataset(torch.load(data_dirpath/'train_X.pt'), torch.load(data_dirpath/'train_y.pt'))
-    val_set = TensorDataset(torch.load(data_dirpath/'val_X.pt'), torch.load(data_dirpath/'val_y.pt'))
-
-    train_dl = DataLoader(train_set, batch_size=batch_size)
-    val_dl = DataLoader(val_set, batch_size=batch_size)
-
-    return train_dl, val_dl
 
 if __name__ == '__main__':
     args = get_args()
@@ -110,59 +56,26 @@ if __name__ == '__main__':
     else:
         raise("Unkwown loss function")
 
-    learning_rate = 2e-4
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 
-    config = dict(
+    run_config = dict(
         dataset = args.data_dir,
         optimizer = "Adam",
-        learning_rate = learning_rate,
+        learning_rate = args.learning_rate,
         batch_size = args.batch_size,
-        architecture = "U-Net",
+        architecture = ARCHITECTURE,
         device = device,
         epochs=args.epochs
     )
 
-    wandb.init(
-        project="ml-downscaling-emulator",
-        tags=["baseline", "U-Net"],
-        config=config
-    )
+    with track_run(EXPERIMENT_NAME, run_config, TAGS) as (wandb_run, tb_writer):
+        # Fit model
+        wandb_run.watch(model, criterion=criterion, log_freq=100)
+        for (epoch, epoch_metrics) in train(train_dl, val_dl, model, criterion, optimizer, args.epochs, device):
+            log_epoch(epoch, epoch_metrics, wandb_run, tb_writer)
 
-    wandb.watch(model, criterion=criterion, log_freq=100)
+            # Checkpoint model
+            if (epoch % 10 == 0) or (epoch + 1 == args.epochs): # every 10th epoch or final one (to be safe)
+                checkpoint_model(model, args.model_checkpoints_dir, epoch)
 
-    set_experiment("ml-downscaling-emulator")
-    set_tags({"model": "U-Net", "purpose": "baseline"})
-    log_param("dataset", args.data_dir)
-    log_param("optimizer", "Adam")
-    log_param("learning_rate", learning_rate)
-    log_param("batch_size", args.batch_size)
-    log_param("architecture", "U-Net")
-    log_param("device", device)
-    log_param("epochs", args.epochs)
-
-    writer = SummaryWriter()
-
-    # Fit model
-    for epoch in range(args.epochs):
-        # Update model based on training data
-        epoch_train_loss = train_epoch(model, train_dl, device, epoch)
-
-        # Compute validation loss
-        epoch_val_loss = val_epoch(model, val_dl, device, epoch)
-
-        logging.info(f"Epoch {epoch}: Train Loss {epoch_train_loss} Val Loss {epoch_val_loss}")
-        wandb.log({"train/loss": epoch_train_loss, "val/loss": epoch_val_loss})
-        log_metric("train/loss",epoch_train_loss, step=epoch)
-        log_metric("val/loss", epoch_val_loss, step=epoch)
-        writer.add_scalar("train/loss", epoch_train_loss, epoch)
-        writer.add_scalar("val/loss", epoch_val_loss, epoch)
-        # Checkpoint model
-        if (epoch % 10 == 0) or (epoch + 1 == args.epochs): # every 10th epoch or final one (to be safe)
-            model_checkpoint_path = args.model_checkpoints_dir / f"model-epoch{epoch}.pth"
-            torch.save(model, model_checkpoint_path)
-            logging.info(f"Epoch {epoch}: Saved model to {model_checkpoint_path}")
-
-    # writer.add_hparams(config, {"train/loss": epoch_train_loss, "val/loss": epoch_val_loss})
-    writer.flush()
     logging.info(f"Finished {os.path.basename(__file__)}")
---
+++ training/__init__.py
@@ -0,0 +1,104 @@
+import argparse
+from contextlib import contextmanager
+import logging
+
+from torch.utils.data import DataLoader, TensorDataset
+
+import wandb
+import mlflow
+from mlflow import log_metric, log_param, set_experiment, set_tags
+from torch.utils.tensorboard import SummaryWriter
+import torch
+
+def train(train_dl, val_dl, model, criterion, optimizer, epochs, device):
+    for epoch in range(epochs):
+        # Update model based on training data
+        epoch_train_loss = train_epoch(train_dl, model, criterion, optimizer, epoch, device)
+
+        # Compute validation loss
+        epoch_val_loss = val_epoch(val_dl, model, criterion, epoch, device)
+
+        epoch_metrics = {"train/loss": epoch_train_loss, "val/loss": epoch_val_loss}
+        yield epoch, epoch_metrics
+
+def train_epoch(dataloader, model, criterion, optimizer, epoch, device):
+    model.train()
+
+    epoch_loss = 0.0
+
+    for i, (batch_X, batch_y) in enumerate(dataloader):
+        loss = train_on_batch(batch_X.to(device), batch_y.to(device), model, criterion, optimizer)
+        epoch_loss += loss.item()
+
+        # Log progress at least every 10th batch
+        if (len(dataloader) <= 10) or ((i+1) % max(len(dataloader)//10,1) == 0):
+            logging.info(f"Epoch {epoch}: Batch {i}: Batch Train Loss {loss.item()} Running Epoch Train Loss {epoch_loss}")
+
+    return epoch_loss
+
+def val_epoch(dataloader, model, criterion, epoch, device):
+    model.eval()
+
+    epoch_val_loss = 0
+    for batch_X, batch_y in dataloader:
+        val_loss = val_on_batch(batch_X.to(device), batch_y.to(device), model, criterion)
+
+        # Progress
+        epoch_val_loss += val_loss.item()
+
+    model.train()
+
+    return epoch_val_loss
+
+def train_on_batch(batch_X, batch_y, model, criterion, optimizer):
+    # Compute prediction and loss
+    outputs_tensor = model(batch_X)
+    loss = criterion(outputs_tensor, batch_y)
+
+    # Backpropagation
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return loss
+
+def val_on_batch(batch_X, batch_y, model, criterion):
+    with torch.no_grad():
+        # Compute prediction and loss
+        outputs_tensor = model(batch_X)
+        loss = criterion(outputs_tensor, batch_y)
+
+    return loss
+
+def load_data(data_dirpath, batch_size):
+    train_set = TensorDataset(torch.load(data_dirpath/'train_X.pt'), torch.load(data_dirpath/'train_y.pt'))
+    val_set = TensorDataset(torch.load(data_dirpath/'val_X.pt'), torch.load(data_dirpath/'val_y.pt'))
+
+    train_dl = DataLoader(train_set, batch_size=batch_size)
+    val_dl = DataLoader(val_set, batch_size=batch_size)
+
+    return train_dl, val_dl
+
+def log_epoch(epoch, epoch_metrics, wandb_run, tb_writer):
+    logging.info(f"Epoch {epoch}: Train Loss {epoch_metrics['train/loss']} Val Loss {epoch_metrics['val/loss']}")
+
+    wandb_run.log(epoch_metrics)
+    mlflow.log_metrics(epoch_metrics, step=epoch)
+    for name, value in epoch_metrics.items():
+        tb_writer.add_scalar(name, value, epoch)
+
+def checkpoint_model(model, model_checkpoints_dir, epoch):
+    model_checkpoint_path = model_checkpoints_dir / f"model-epoch{epoch}.pth"
+    torch.save(model, model_checkpoint_path)
+    logging.info(f"Epoch {epoch}: Saved model to {model_checkpoint_path}")
+
+@contextmanager
+def track_run(experiment_name, config, tags):
+    with wandb.init(project=experiment_name, tags=tags, config=config) as wandb_run:
+        set_experiment(experiment_name)
+        with mlflow.start_run() :
+            mlflow.set_tags({ key: True for key in tags})
+            mlflow.log_params(config)
+
+            with SummaryWriter() as tb_writer:
+                yield wandb_run, tb_writer
