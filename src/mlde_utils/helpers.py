c5e939b659d5e5a82c8677973acd86093f5289c2
Henry Addison <henryaddison@users.noreply.github.com>
2021-12-02 14:14:55 +0000

    use combine_by_coords in load_dataset helper

--- src/mlde_utils/helpers.py
+++ src/mlde_utils/helpers.py
@@ -40,15 +40,15 @@ MODEL2RES = {
 def data_filepath(horizontal_desc, source_model, variable, year, rcp="rcp85", ensemble_member="01", temp_res="day"):
     source_res = MODEL2RES[source_model]
     year_range = f"{year}1201-{year+1}1130"
-    
+
     file_name = f"{variable}_{rcp}_land-{source_model}_uk_{source_res}_{ensemble_member}_{temp_res}_{year_range}.nc"
     if horizontal_desc == "2.2km":
         base_path = "../../../../data"
     else:
         base_path = "../../../../derived_data"
-    
+
     return f"{base_path}/{horizontal_desc}/{rcp}/{ensemble_member}/{variable}/{temp_res}/{file_name}"
-    
+
 def load_dataset(horizontal_desc, source_model, variable, years, rcp="rcp85", ensemble_member="01", temp_res="day"):
     filepaths = [data_filepath(horizontal_desc, source_model, variable, year, rcp, ensemble_member, temp_res) for year in years]
-    return xr.concat([xr.load_dataset(filepath) for filepath in filepaths], dim="time")
+    return xr.combine_by_coords([xr.load_dataset(filepath, decode_cf=True, decode_coords=True) for filepath in filepaths], combine_attrs="drop_conflicts", coords="all")
