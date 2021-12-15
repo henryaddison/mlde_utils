946b29860ea72d118094a5753cd7c3e688c46423
Henry Addison <henryaddison@users.noreply.github.com>
2021-12-15 11:22:07 +0000

    move tensor to cpu before turning into numpy array

--- src/mlde_utils/evaluation/__init__.py
+++ src/mlde_utils/evaluation/__init__.py
@@ -31,7 +31,7 @@ def predict(model, test_set, sample=None):
 
     model.to(device=device)
 
-    pred = np.concatenate([model(batch_X.to(device)).squeeze().detach().numpy() for (batch_X, _) in test_dl])
+    pred = np.concatenate([model(batch_X.to(device)).squeeze().detach().cpu().numpy() for (batch_X, _) in test_dl])
 
     ds = xr.Dataset(data_vars={key: test_set.data_vars[key] for key in ["time_bnds", "grid_latitude_bnds", "grid_longitude_bnds", "rotated_latitude_longitude"]}, coords=test_set.coords, attrs={})
     ds['pr'] = xr.DataArray(pred, dims=["time", "grid_latitude", "grid_longitude"])
