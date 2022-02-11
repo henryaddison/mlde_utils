2a5183cb42e1a688707bd07b33d60224931cf017
Henry Addison <henryaddison@users.noreply.github.com>
2022-02-11 23:20:14 +0000

    allow selecting of a 64x64 subregion centred on London

--- src/mlde_utils/preprocessing/select_region.py
+++ src/mlde_utils/preprocessing/select_region.py
@@ -9,17 +9,15 @@ class SelectRegion:
     # expected index ranges for LONDON_IN_CPM {"grid_latitude": range(142, 196), "grid_longitude": range(337, 391)}
     LONDON_IN_CPM = {"grid_latitude": slice(-1.51995003, -0.45995), "grid_longitude": slice(361.03076172, 362.09075928)}
 
+    LONDON_IN_CPM_64x64 = dict(grid_longitude=slice(360.86076, 362.13074), grid_latitude=slice(-1.57995, -0.31995))
+
     # expected index ranges for LONDON_IN_GCM {"projection_x_coordinate": [12, 13], "projection_y_coordinate": [4, 5]}
     LONDON_IN_GCM = {"projection_x_coordinate": slice(510000., 570000.), "projection_y_coordinate": slice(150000., 210000.)}
 
-    def __init__(self, input_filepath, output_filepath, subregion_defn) -> None:
-        self.input_filepath = input_filepath
-        self.output_filepath = output_filepath
+    def __init__(self, subregion_defn) -> None:
         self.subregion_defn = subregion_defn
 
-    def run(self):
-            logger.info(f"Selecting region of {self.input_filepath}")
-            da = xr.load_dataset(self.input_filepath)
+    def run(self, ds):
+        logger.info(f"Selecting region ")
 
-            subregion_da = da.sel(self.subregion_defn)
-            subregion_da.to_netcdf(self.output_filepath)
+        return ds.sel(self.subregion_defn)
