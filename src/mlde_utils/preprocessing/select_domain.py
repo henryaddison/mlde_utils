d1531dfc41bd676cc01b29e64befade6ec6dc85b
Henry Addison <henryaddison@users.noreply.github.com>
2022-02-14 10:27:36 +0000

    use domain rather than region consistently
    as definition of a spatial geographic area

--- src/mlde_utils/preprocessing/select_domain.py
+++ src/mlde_utils/preprocessing/select_domain.py
@@ -4,7 +4,7 @@ import xarray as xr
 
 logger = logging.getLogger(__name__)
 
-class SelectRegion:
+class SelectDomain:
 
     # expected index ranges for LONDON_IN_CPM {"grid_latitude": range(142, 196), "grid_longitude": range(337, 391)}
     LONDON_IN_CPM = {"grid_latitude": slice(-1.51995003, -0.45995), "grid_longitude": slice(361.03076172, 362.09075928)}
@@ -14,10 +14,10 @@ class SelectRegion:
     # expected index ranges for LONDON_IN_GCM {"projection_x_coordinate": [12, 13], "projection_y_coordinate": [4, 5]}
     LONDON_IN_GCM = {"projection_x_coordinate": slice(510000., 570000.), "projection_y_coordinate": slice(150000., 210000.)}
 
-    def __init__(self, subregion_defn) -> None:
-        self.subregion_defn = subregion_defn
+    def __init__(self, subdomain_defn) -> None:
+        self.subdomain_defn = subdomain_defn
 
     def run(self, ds):
-        logger.info(f"Selecting region ")
+        logger.info(f"Selecting subdomain {self.subdomain_defn}")
 
-        return ds.sel(self.subregion_defn)
+        return ds.sel(self.subdomain_defn)
