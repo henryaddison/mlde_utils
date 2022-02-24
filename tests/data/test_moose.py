a6f21d3ac7028da27f81cf2ab3b36e54d656d199
Henry Addison <henryaddison@users.noreply.github.com>
2022-02-24 14:07:46 +0000

    fix moose tests

--- tests/data/test_moose.py
+++ tests/data/test_moose.py
@@ -1,11 +1,11 @@
 from ml_downscaling_emulator.data.moose import moose_path, select_query
 
 def test_moose_path():
-    assert moose_path(variable="pr", year=1981) == "moose:crum/mi-bb171/apb.pp"
+    assert moose_path(variable="lsrain", year=1981) == "moose:crum/mi-bb171/apa.pp"
 
 def test_select_query():
     year = 1981
-    variable = "1.5mtmean"
+    variable = "tmean150cm"
 
     expected = """
 begin
