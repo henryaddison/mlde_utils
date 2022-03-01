a8d9551bce503338dc3b4aeadb43d1c69dd355e6
Henry Addison <henryaddison@users.noreply.github.com>
2022-03-01 16:19:40 +0000

    change domain selection to centre around a point using indices
    rather than using hard-coded values for lat and long which may not match the actual grid given (but probably should)
    
    and include assertions about the size of the output after selecting sub-domain to avoid problem in the future

--- src/mlde_utils/preprocessing/select_domain.py
+++ src/mlde_utils/preprocessing/select_domain.py
@@ -1,6 +1,8 @@
 import logging
 
-import xarray as xr
+import numpy as np
+
+from ml_downscaling_emulator.utils import cp_model_rotated_pole, platecarree
 
 logger = logging.getLogger(__name__)
 
@@ -11,13 +13,26 @@ class SelectDomain:
 
     LONDON_IN_CPM_64x64 = dict(grid_longitude=slice(360.86076, 362.13074), grid_latitude=slice(-1.57995, -0.31995))
 
+    LONDON_LONG_LAT  = (-0.118092, 51.509865)
+    LONDON_RP_LONG_LAT = cp_model_rotated_pole.transform_point(*LONDON_LONG_LAT, src_crs=platecarree)
+
     # expected index ranges for LONDON_IN_GCM {"projection_x_coordinate": [12, 13], "projection_y_coordinate": [4, 5]}
     LONDON_IN_GCM = {"projection_x_coordinate": slice(510000., 570000.), "projection_y_coordinate": slice(150000., 210000.)}
 
-    def __init__(self, subdomain_defn) -> None:
-        self.subdomain_defn = subdomain_defn
+    def __init__(self, subdomain) -> None:
+        self.subdomain = subdomain
 
     def run(self, ds):
-        logger.info(f"Selecting subdomain {self.subdomain_defn}")
+        logger.info(f"Selecting subdomain {self.subdomain}")
+        if self.subdomain == "london":
+            centre_ds = ds.sel(grid_longitude=360.0+self.LONDON_RP_LONG_LAT[0], grid_latitude=self.LONDON_RP_LONG_LAT[1], method="nearest")
 
-        return ds.sel(self.subdomain_defn)
+            centre_long_idx = np.where(ds.grid_longitude.values == centre_ds.grid_longitude.values)
+            centre_lat_idx = np.where(ds.grid_latitude.values == centre_ds.grid_latitude.values)
+
+            ds = ds.sel(grid_longitude=slice(ds.grid_longitude[centre_long_idx-31].values, ds.grid_longitude[centre_long_idx+32].values), grid_latitude=slice(ds.grid_latitude[centre_lat_idx-31].values, ds.grid_latitude[centre_lat_idx+32].values))
+
+            return ds
+            # london_cp_rp_long_lat_idx = (ds.coord('grid_longitude').nearest_neighbour_index(london_rp_long_lat[0]+360), ds.coord('grid_latitude').nearest_neighbour_index(london_rp_long_lat[1]))
+
+        # return ds.sel(self.subdomain_defn)
