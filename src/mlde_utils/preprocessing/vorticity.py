21c81998662677aa85b20a77d5b241cebe0e043c
Henry Addison <henryaddison@users.noreply.github.com>
2022-03-07 20:14:24 +0000

    use metpy for vorticity - BUT doesn't understand time dim yet

--- src/mlde_utils/preprocessing/vorticity.py
+++ src/mlde_utils/preprocessing/vorticity.py
@@ -1,62 +1,17 @@
-from windspharm.xarray import to3d, get_apiorder, inspect_gridtype
-import windspharm
+import metpy.calc as mpcalc
 import xarray as xr
 
 class Vorticity:
     def __init__(self):
-        self.gridtype = 'regular'
+        pass
 
     def run(self, ds):
-        u = ds['xwind']
-        v = ds['ywind']
+        dx, dy = mpcalc.lat_lon_grid_deltas(ds.grid_longitude.values, ds.grid_latitude.values)
 
-        lat = u.coords['grid_latitude']
-        lat_dim = u.dims.index(lat.name)
+        ds['vorticity'] = mpcalc.vorticity(ds.isel(time=10)['xwind'], ds.isel(time=10)['ywind'], dx=dx, dy=dy)
 
-        lon = u.coords['grid_longitude']
-        lon_dim = u.dims.index(lon.name)
-
-        if lat.values[0] < lat.values[1]:
-            u = self._reverse(u, lat_dim)
-            v = self._reverse(v, lat_dim)
-
-            lat = u.coords['grid_latitude']
-            lat_dim = u.dims.index(lat.name)
-
-        apiorder, _ = get_apiorder(u.ndim, lat_dim, lon_dim)
-        apiorder = [u.dims[i] for i in apiorder]
-
-        reorder = u.dims
-        u = u.copy().transpose(*apiorder)
-        v = v.copy().transpose(*apiorder)
-        # Reshape the raw data and input into the API.
-        ishape = u.shape
-        coords = [u.coords[name] for name in u.dims]
-
-        u = to3d(u.values)
-        v = to3d(v.values)
-
-        api = windspharm.standard.VectorWind(u, v, gridtype=self.gridtype)
-
-        vrt = api.vorticity()
-
-        ds['vorticity'] = self._metadata(vrt, 'vorticity', ishape, coords, reorder,
-            units='s**-1',
-            standard_name='atmosphere_relative_vorticity',
-            long_name='relative_vorticity')
+        # units='s**-1',
+        # standard_name='atmosphere_relative_vorticity',
+        # long_name='relative_vorticity'
 
         return ds
-
-    def _reverse(self, array, dim):
-        """Reverse an `xarray.DataArray` along a given dimension."""
-        slicers = [slice(0, None)] * array.ndim
-        slicers[dim] = slice(-1, None, -1)
-        return array[tuple(slicers)]
-
-    def _metadata(self, var, name, ishape, coords, reorder, **attributes):
-        var = var.reshape(ishape)
-        var = xr.DataArray(var, coords=coords, name=name)
-        var = var.transpose(*reorder)
-        for attr, value in attributes.items():
-            var.attrs[attr] = value
-        return var
