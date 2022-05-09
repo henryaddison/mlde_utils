3f68b9a04e6e3b15704a4a5d8186abd63265a3a0
Henry Addison <henryaddison@users.noreply.github.com>
2022-05-09 15:32:39 +0100

    update vorticity preprocessing to handle GCM data
    so behaves differently for lat-lon grid mapping of GCM data rather than rotated pole grid mapping of CPM data
    and determines on fly which to use based on attributes of Dataset

--- src/mlde_utils/preprocessing/vorticity.py
+++ src/mlde_utils/preprocessing/vorticity.py
@@ -10,18 +10,18 @@ class Vorticity:
 
     def run(self, ds):
         logger.info(f"Computing vorticity from xwind, ywind")
-        dx, dy =  mpcalc.lat_lon_grid_deltas(ds.grid_longitude.values, ds.grid_latitude.values)
-        # make sure grid deltas broadcast properly over time dimension - https://stackoverflow.com/a/55012247
-        dx = dx[None, :]
-        dy = dy[None, :]
+        if ds['xwind'].attrs['grid_mapping'] == "latitude_longitude":
+            vort_da = mpcalc.vorticity(ds['xwind'], ds['ywind'])
+        elif ds['xwind'].attrs['grid_mapping'] == "rotated_latitude_longitude":
+            dx, dy =  mpcalc.lat_lon_grid_deltas(ds.grid_longitude.values, ds.grid_latitude.values)
+            # make sure grid deltas broadcast properly over time dimension - https://stackoverflow.com/a/55012247
+            dx = dx[None, :]
+            dy = dy[None, :]
+            vort_da = mpcalc.vorticity(ds['xwind'], ds['ywind'], dx=dx, dy=dy)
 
-        ds['vorticity850'] = mpcalc.vorticity(ds['xwind'], ds['ywind'], dx=dx, dy=dy)
+        vort_da = vort_da.assign_attrs(grid_mapping=ds['xwind'].attrs['grid_mapping'], units="s-1", standard_name="atmosphere_relative_vorticity", long_name="relative_vorticity")
+        ds['vorticity850'] = vort_da
 
         ds = ds.drop_vars(['xwind', 'ywind'])
-
-        # units='s**-1',
-        # standard_name='atmosphere_relative_vorticity',
-        # long_name='relative_vorticity'
-        # grid_mapping='rotated_latitude_longitude'}
 
         return ds
