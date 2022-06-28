baa3045b8e97d3efde519f8800a1a8d0c339a1c5
Henry Addison <henryaddison@users.noreply.github.com>
2022-06-28 13:14:05 +0100

    make a helper function for plotting grids

--- src/mlde_utils/utils/__init__.py
+++ src/mlde_utils/utils/__init__.py
@@ -1,4 +1,12 @@
 import cartopy.crs as ccrs
+import matplotlib
 
 cp_model_rotated_pole = ccrs.RotatedPole(pole_longitude=177.5, pole_latitude=37.5)
 platecarree = ccrs.PlateCarree()
+
+
+def plot_grid(da, ax, title="", norm=matplotlib.colors.LogNorm(), cmap='Blues', add_colorbar=False, **kwargs):
+    da.plot.pcolormesh(ax=ax, norm=norm, add_colorbar=add_colorbar, cmap=cmap, **kwargs)
+    ax.set_title(title, fontsize=16)
+    ax.coastlines()
+    ax.gridlines(draw_labels={"bottom": "x", "left": "y"}, x_inline=False, y_inline=False, xlabel_style=dict(fontsize=12), ylabel_style=dict(fontsize=12))
