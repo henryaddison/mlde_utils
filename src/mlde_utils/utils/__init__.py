004b1490bd53e5e3a21441c913b28ce5b4f246d9
Henry Addison <henryaddison@users.noreply.github.com>
2023-06-19 15:33:10 +0100

    allow evaluation to use a list of ensemble members

--- src/mlde_utils/utils/__init__.py
+++ src/mlde_utils/utils/__init__.py
@@ -15,49 +15,62 @@ def open_samples_ds(
     dataset_name,
     input_xfm_key,
     split,
+    ensemble_members,
     num_samples,
     deterministic,
 ):
-    samples_dir = samples_path(
-        workdir=workdir_path(run_name),
-        checkpoint=checkpoint_id,
-        input_xfm=input_xfm_key,
-        dataset=dataset_name,
-        split=split,
-    )
-    sample_files_list = samples_glob(samples_dir)
-    if len(sample_files_list) == 0:
-        raise RuntimeError(f"{samples_dir} has no sample files")
 
-    if deterministic:
-        ds = xr.open_dataset(sample_files_list[0])
-    else:
-        sample_files_list = sample_files_list[:num_samples]
-        if len(sample_files_list) < num_samples:
-            raise RuntimeError(
-                f"{samples_dir} does not have {num_samples} sample files"
-            )
-        ds = xr.concat(
-            [xr.open_dataset(sample_filepath) for sample_filepath in sample_files_list],
-            dim="sample_id",
-        ).isel(sample_id=range(num_samples))
+    per_em_datasets = []
+    for ensemble_member in ensemble_members:
+        samples_dir = samples_path(
+            workdir=workdir_path(run_name),
+            checkpoint=checkpoint_id,
+            input_xfm=input_xfm_key,
+            dataset=dataset_name,
+            split=split,
+            ensemble_member=ensemble_member,
+        )
+        sample_files_list = samples_glob(samples_dir)
+        if len(sample_files_list) == 0:
+            raise RuntimeError(f"{samples_dir} has no sample files")
 
+        if deterministic:
+            em_ds = xr.open_dataset(sample_files_list[0])
+        else:
+            sample_files_list = sample_files_list[:num_samples]
+            if len(sample_files_list) < num_samples:
+                raise RuntimeError(
+                    f"{samples_dir} does not have {num_samples} sample files"
+                )
+            em_ds = xr.concat(
+                [
+                    xr.open_dataset(sample_filepath)
+                    for sample_filepath in sample_files_list
+                ],
+                dim="sample_id",
+            ).isel(sample_id=range(num_samples))
+
+        per_em_datasets.append(em_ds)
+
+    ds = xr.concat(per_em_datasets, dim="ensemble_member")
     ds["pred_pr"] = si_to_mmday(ds, "pred_pr")
 
     return ds
 
 
-def open_split_ds(dataset_name, split):
-    ds = xr.open_dataset(dataset_split_path(dataset_name, split))
+def open_split_ds(dataset_name, split, ensemble_members):
+    ds = xr.open_dataset(dataset_split_path(dataset_name, split)).sel(
+        ensemble_members=ensemble_members
+    )
     ds["target_pr"] = si_to_mmday(ds, "target_pr")
 
     return ds
 
 
-def open_merged_split_datasets(sample_runs, split):
+def open_merged_split_datasets(sample_runs, split, ensemble_members):
     return xr.merge(
         [
-            open_split_ds(dataset_name, split)
+            open_split_ds(dataset_name, split, ensemble_members)
             for dataset_name in set(
                 [sample_run["dataset"] for sample_run in sample_runs]
             )
@@ -66,7 +79,7 @@ def open_merged_split_datasets(sample_runs, split):
     )
 
 
-def open_concat_sample_datasets(sample_runs, split, samples_per_run):
+def open_concat_sample_datasets(sample_runs, split, ensemble_members, samples_per_run):
     samples_das = [
         open_samples_ds(
             run_name=sample_run["fq_model_id"],
@@ -74,6 +87,7 @@ def open_concat_sample_datasets(sample_runs, split, samples_per_run):
             dataset_name=sample_run["dataset"],
             input_xfm_key=sample_run["input_xfm"],
             split=split,
+            ensemble_members=ensemble_members,
             num_samples=samples_per_run,
             deterministic=sample_run["deterministic"],
         )["pred_pr"]
@@ -87,9 +101,11 @@ def open_concat_sample_datasets(sample_runs, split, samples_per_run):
     return samples_ds
 
 
-def prep_eval_data(sample_runs, split, samples_per_run=3):
-    samples_ds = open_concat_sample_datasets(sample_runs, split, samples_per_run)
+def prep_eval_data(sample_runs, split, ensemble_members, samples_per_run=3):
+    samples_ds = open_concat_sample_datasets(
+        sample_runs, split, ensemble_members, samples_per_run
+    )
 
-    eval_ds = open_merged_split_datasets(sample_runs, split)
+    eval_ds = open_merged_split_datasets(sample_runs, split, ensemble_members)
 
     return xr.merge([samples_ds, eval_ds], join="inner", compat="override")
