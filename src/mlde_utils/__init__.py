442f920b9782890eec2001487174ccd4b5f4d4cf
Henry Addison <henryaddison@users.noreply.github.com>
2023-06-20 14:45:33 +0100

    store default ensemble member const in shared lib

--- src/mlde_utils/__init__.py
+++ src/mlde_utils/__init__.py
@@ -9,6 +9,8 @@ import cartopy.crs as ccrs
 cp_model_rotated_pole = ccrs.RotatedPole(pole_longitude=177.5, pole_latitude=37.5)
 platecarree = ccrs.PlateCarree()
 
+DEFAULT_ENSEMBLE_MEMBER = "01"
+
 
 class VariableMetadata:
     def __init__(
