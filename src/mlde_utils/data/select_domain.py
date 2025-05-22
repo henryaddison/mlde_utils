31aa528509b7ed3cd86bcd74acb354d3bdb93abb
Henry Addison <henryaddison@users.noreply.github.com>
2025-05-22 14:01:48 +0100

    allow selecting of domain for both rot. pole and standard lon lat

--- src/mlde_utils/data/select_domain.py
+++ src/mlde_utils/data/select_domain.py
@@ -26,26 +26,45 @@ class SelectDomain:
         for domain_name, lon_lat in DOMAIN_CENTRES_LON_LAT.items()
     }
 
-    def __init__(self, subdomain, size=64) -> None:
+    def __init__(self, subdomain, grid="cpm", size=64) -> None:
         self.subdomain = subdomain
+        self.grid = grid
         self.size = size
 
     def run(self, ds):
         logger.info(f"Selecting subdomain {self.subdomain}")
+        if self.grid == "cpm":
+            centre_xy = self.DOMAIN_CENTRES_RP_LONG_LAT[self.subdomain]
+            query = dict(
+                grid_longitude=360.0 + centre_xy[0],
+                grid_latitude=centre_xy[1],
+            )
+        elif self.grid == "gcm":
+            centre_xy = self.DOMAIN_CENTRES_LON_LAT[self.subdomain]
+            query = dict(
+                longitude=centre_xy[0],
+                latitude=centre_xy[1],
+            )
+        else:
+            raise ValueError(f"Unknown grid type: {self.grid}")
 
-        centre_rp_lon_lat = self.DOMAIN_CENTRES_RP_LONG_LAT[self.subdomain]
-        centre_ds = ds.sel(
-            grid_longitude=360.0 + centre_rp_lon_lat[0],
-            grid_latitude=centre_rp_lon_lat[1],
-            method="nearest",
-        )
-
-        centre_long_idx = np.where(
-            ds.grid_longitude.values == centre_ds.grid_longitude.values
-        )[0].item()
-        centre_lat_idx = np.where(
-            ds.grid_latitude.values == centre_ds.grid_latitude.values
-        )[0].item()
+        centre_ds = ds.sel(query, method="nearest")
+        if self.grid == "cpm":
+            centre_long_idx = np.where(
+                ds.grid_longitude.values == centre_ds.grid_longitude.values
+            )[0].item()
+            centre_lat_idx = np.where(
+                ds.grid_latitude.values == centre_ds.grid_latitude.values
+            )[0].item()
+        elif self.grid == "gcm":
+            centre_long_idx = np.where(
+                ds.longitude.values == centre_ds.longitude.values
+            )[0].item()
+            centre_lat_idx = np.where(ds.latitude.values == centre_ds.latitude.values)[
+                0
+            ].item()
+        else:
+            raise ValueError(f"Unknown grid type: {self.grid}")
 
         radius = self.size - 1
         left_length = math.floor(radius / 2.0)
@@ -53,15 +72,29 @@ class SelectDomain:
         down_length = math.floor(radius / 2.0)
         up_length = math.ceil(radius / 2.0)
 
-        ds = ds.sel(
-            grid_longitude=slice(
-                ds.grid_longitude[centre_long_idx - left_length].values,
-                ds.grid_longitude[centre_long_idx + right_length].values,
-            ),
-            grid_latitude=slice(
-                ds.grid_latitude[centre_lat_idx - down_length].values,
-                ds.grid_latitude[centre_lat_idx + up_length].values,
-            ),
-        )
+        if self.grid == "cpm":
+            ds = ds.sel(
+                grid_longitude=slice(
+                    ds.grid_longitude[centre_long_idx - left_length].values,
+                    ds.grid_longitude[centre_long_idx + right_length].values,
+                ),
+                grid_latitude=slice(
+                    ds.grid_latitude[centre_lat_idx - down_length].values,
+                    ds.grid_latitude[centre_lat_idx + up_length].values,
+                ),
+            )
+        elif self.grid == "gcm":
+            ds = ds.sel(
+                longitude=slice(
+                    ds.longitude[centre_long_idx - left_length].values,
+                    ds.longitude[centre_long_idx + right_length].values,
+                ),
+                latitude=slice(
+                    ds.latitude[centre_lat_idx - down_length].values,
+                    ds.latitude[centre_lat_idx + up_length].values,
+                ),
+            )
+        else:
+            raise ValueError(f"Unknown grid type: {self.grid}")
 
         return ds
