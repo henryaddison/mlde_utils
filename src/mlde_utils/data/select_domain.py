24628b9d2ab2f54d23060fe9030a725dafc8bb0d
Henry Addison <henryaddison@users.noreply.github.com>
2025-05-30 16:33:09 +0100

    roll GCM and CPM domain selection into same action
    using cf_xarray to ease the logic

--- src/mlde_utils/data/select_domain.py
+++ src/mlde_utils/data/select_domain.py
@@ -5,10 +5,12 @@ import cf_xarray  # noqa: F401
 import numpy as np
 
 from mlde_utils import cp_model_rotated_pole, platecarree
+from mlde_utils.data import register_action
 
 logger = logging.getLogger(__name__)
 
 
+@register_action(name="select-subdomain")
 class SelectDomain:
 
     DOMAIN_CENTRES_LON_LAT = {
@@ -27,20 +29,20 @@ class SelectDomain:
         for domain_name, (lon, lat) in DOMAIN_CENTRES_LON_LAT.items()
     }
 
-    def __init__(self, subdomain, size=64) -> None:
-        self.subdomain = subdomain
+    def __init__(self, domain, size=64) -> None:
+        self.domain = domain
         self.size = size
 
-    def run(self, ds):
-        logger.info(f"Selecting subdomain {self.subdomain}")
+    def __call__(self, ds):
+        logger.info(f"Selecting subdomain {self.domain}")
         if "rotated_latitude_longitude" in ds.cf.grid_mapping_names:
-            centre_xy = self.DOMAIN_CENTRES_RP_LONG_LAT[self.subdomain]
+            centre_xy = self.DOMAIN_CENTRES_RP_LONG_LAT[self.domain]
             query = dict(
                 X=centre_xy[0],
                 Y=centre_xy[1],
             )
         elif "latitude_longitude" in ds.cf.grid_mapping_names:
-            centre_xy = self.DOMAIN_CENTRES_LON_LAT[self.subdomain]
+            centre_xy = self.DOMAIN_CENTRES_LON_LAT[self.domain]
             query = dict(
                 X=centre_xy[0],
                 Y=centre_xy[1],
@@ -49,24 +51,25 @@ class SelectDomain:
             raise ValueError(f"Unknown grid type: {self.grid}")
 
         centre_ds = ds.cf.sel(query, method="nearest")
-        centre_long_idx = np.where(ds.cf.X.values == centre_ds.cf.X.values)[0].item()
-        centre_lat_idx = np.where(ds.cf.Y.values == centre_ds.cf.Y.values)[0].item()
+        centre_long_idx = np.where(ds.cf["X"].values == centre_ds.cf["X"].values)[
+            0
+        ].item()
+        centre_lat_idx = np.where(ds.cf["Y"].values == centre_ds.cf["Y"].values)[
+            0
+        ].item()
 
-        radius = self.size - 1
-        left_length = math.floor(radius / 2.0)
-        right_length = math.ceil(radius / 2.0)
-        down_length = math.floor(radius / 2.0)
-        up_length = math.ceil(radius / 2.0)
+        radius = self.size / 2.0
+        ledge_idx = math.ceil(centre_long_idx - radius)
+        redge_idx = math.ceil(centre_long_idx + radius)
+        bedge_idx = math.ceil(centre_lat_idx - radius)
+        tedge_idx = math.ceil(centre_lat_idx + radius)
 
-        ds = ds.cf.sel(
-            X=slice(
-                ds.cf.X[centre_long_idx - left_length].values,
-                ds.cf.X[centre_long_idx + right_length].values,
-            ),
-            Y=slice(
-                ds.cf.Y[centre_lat_idx - down_length].values,
-                ds.cf.Y[centre_lat_idx + up_length].values,
-            ),
+        ds = ds.cf.isel(
+            X=slice(ledge_idx, redge_idx),
+            Y=slice(bedge_idx, tedge_idx),
         )
+
+        assert len(ds.cf["X"]) == self.size
+        assert len(ds.cf["Y"]) == self.size
 
         return ds
