c8a7b76d665afb0f85a49472ca0fe6c1c984f502
Henry Addison <henryaddison@users.noreply.github.com>
2025-06-06 15:15:02 +0100

    add logging that was previously part of variable creation loop

--- src/mlde_utils/data/diff.py
+++ src/mlde_utils/data/diff.py
@@ -2,6 +2,7 @@
 Create a new dataset with a variable formed from the difference of two given
 """
 
+import logging
 from mlde_utils.data.actions_registry import register_action
 
 
@@ -13,6 +14,7 @@ class Diff:
         self.new_variable = new_variable
 
     def __call__(self, ds):
+        logging.info(f"Difference between {self.left} and {self.right}")
         ds[self.new_variable] = ds[self.left] - ds[self.right]
         ds = ds.drop_vars([self.left, self.right])
         return ds
--- src/mlde_utils/data/sum.py
+++ src/mlde_utils/data/sum.py
@@ -2,6 +2,7 @@
 Create a new dataset with a variable formed from the sum of the given ones
 """
 
+import logging
 from mlde_utils.data.actions_registry import register_action
 
 
@@ -12,6 +13,7 @@ class Sum:
         self.new_variable = new_variable
 
     def __call__(self, ds):
+        logging.info(f"Summing {self.variables}")
         ds = ds.assign(
             {self.new_variable: lambda x: sum([x[var] for var in self.variables])}
         )
